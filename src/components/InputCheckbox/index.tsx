f6683c7d866c6a77a54558f5a775155bb5b041a6
Parth Modi <62423981+parthmodi152@users.noreply.github.com>
2025-03-04 16:01:17 +0000

    bug2 fix: Clicking the checkbox toggles its value

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -8,6 +8,8 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
       <label
+        // bug2 fix: Label wasn't bound to the hidden checkbox input
+        htmlFor={inputId}
         className={classNames("RampInputCheckbox--label", {
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
